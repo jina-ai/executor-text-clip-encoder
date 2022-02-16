327a900c68a3acd7b2f2ff81956aa7e7f8901612
Sami Jaghouar <sami.jaghouar@jina.ai>
2022-02-16 09:57:44 +0100

    fix: delete return results in post

--- tests/integration/test_with_flow.py
+++ tests/integration/test_with_flow.py
@@ -13,12 +13,11 @@ def test_integration(request_size: int):
     docs = DocumentArray(
         [Document(text='just some random text here') for _ in range(50)]
     )
-    with Flow(return_results=True).add(uses=CLIPTextEncoder) as flow:
+    with Flow().add(uses=CLIPTextEncoder) as flow:
         da = flow.post(
             on='/index',
             inputs=docs,
             request_size=request_size,
-            return_results=True,
         )
 
     assert len(da) == 50
